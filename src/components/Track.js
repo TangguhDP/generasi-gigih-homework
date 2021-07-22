b52e2b719ba76640f0fd6bd5317c3b0fe125537d
Tangguh Pramono <tangguhmyname@gmail.com>
2021-07-22 23:07:02 +0700

    Adding select and deselect track

--- src/components/Track.js
+++ src/components/Track.js
@@ -4,15 +4,19 @@ import { FlatButton } from "./Buttons";
 import ImgCover from "./ImgCover";
 import InfoTextWithTitle from "./InfoTextWithTitle";
 
-export default function Track(props) {
+export default function Track({height, imgUrl, title, artistName, albumName, selected, onSelected, onDeselected}) {
   return (
     <div className="track-card">
-      <ImgCover height={props.height} url={props.imgUrl} />
+      <ImgCover height={height} url={imgUrl} />
       <div className="track-info">
-        <InfoTextWithTitle title="Title" value={props.title} />
-        <InfoTextWithTitle title="Artist" value={props.artistName} />
-        <InfoTextWithTitle title="Album" value={props.albumName} />
-        <FlatButton className="green-button" value="Select" />
+        <InfoTextWithTitle title="Title" value={title} />
+        <InfoTextWithTitle title="Artist" value={artistName} />
+        <InfoTextWithTitle title="Album" value={albumName} />
+        <FlatButton
+          className="green-button"
+          value={selected ? "Deselect" : "Select"}
+          onClick={selected ? onDeselected : onSelected}
+        />
       </div>
     </div>
   );
